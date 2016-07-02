997f32ee777b210a4429391fe32cc899efb17e77
Daniel J. Summers <daniel.summers.2415@gmail.com>
2016-07-01 22:41:05 -0500

    v0.8.4 - cookies and config
    Moved logic to get the session Id from the cookie and store the session
    Id in a cookie from RethinkDB project; created configuration object to
    hold non-RethinkDB-specific options

--- src/Nancy.Session.Persistable/PersistableSession.fs
+++ src/Nancy.Session.Persistable/PersistableSession.fs
@@ -1,10 +1,14 @@
 ﻿namespace Nancy.Session.Persistable
 
+open Nancy
+open Nancy.Cryptography
 open Nancy.Session
 open Newtonsoft.Json
 open System.Collections.Generic
+open System
 
 /// Interface for a persistable session
+[<AllowNullLiteral>]
 type IPersistableSession = 
   inherit ISession
 
@@ -32,3 +36,24 @@ type BasePersistableSession(values : IDictionary<string, obj>) =
   /// <returns>The value (or a default of that type if the value is not set or does not deserialize)</returns>
   abstract Get<'T> : string -> 'T
   default this.Get<'T> key = (this :> IPersistableSession).Get<'T> key
+
+
+/// Base configuration options for persistable sessions
+[<AllowNullLiteral>]
+type BasePersistableSessionConfiguration(cryptoConfig : CryptographyConfiguration) as this =
+  inherit CookieBasedSessionsConfiguration(cryptoConfig)
+  
+  do
+    this.Serializer <- DefaultObjectSerializer()
+
+  /// Gets or sets whether to use rolling sessions (expiry based on inactivity) or not (expiry based on creation)
+  member val UseRollingSessions = true with get, set
+
+  /// Gets or sets the session expiry period
+  member val Expiry = TimeSpan(2, 0, 0) with get, set
+
+  /// Gets or sets the frequency with which expired sessions are removed from storage
+  member val ExpiryCheckFrequency = TimeSpan(0, 1, 0) with get, set
+
+  /// Get the validity state of the session
+  override this.IsValid with get() = base.IsValid
